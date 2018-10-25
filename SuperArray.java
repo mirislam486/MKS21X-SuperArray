bf39c15af2b7772fc616031ba511dbe04d8ad383
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-10-25 14:27:37 -0400

    Add files via upload

---
+++ SuperArray.java
@@ -0,0 +1,59 @@
+public class SuperArray{
+
+  String[] arr;
+  public SuperArray(String[] newAr){
+    arr = new String[newAr, length]
+    for(int i; i < newAr.length, i++){
+      arr[i] = newA[i]
+    }
+  }
+
+}
+  public void clear(){
+    
+  }
+
+  public int size(){
+
+  }
+
+  public boolean inEmpty(){
+
+  }
+
+  public boolean add(String element){
+
+  }
+
+  public String get(int index){
+
+  }
+
+  public String set(int index, String element){
+
+  }
+
+  public String toString(){
+
+  }
+
+  public boolean contains(String element){
+
+  }
+
+  public void add(int index, String element){
+
+  }
+
+  public int lastIndexOf(String element){
+
+  }
+
+  public String remove(int index){
+
+  }
+
+  public boolean remove(String element){
+
+  }
+}
